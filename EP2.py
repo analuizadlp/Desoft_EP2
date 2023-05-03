8c9f597553a8a74f226318d4f9b2ae6280322a0e
Ana Luiza de la Peña <analblp@al.insper.edu.br>
2023-05-03 08:11:31 -0300

    preenche frota

--- EP2.py
+++ EP2.py
@@ -6,3 +6,11 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
         else:
             posicoes.append([linha+i, coluna])
     return posicoes
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes)
+    else:
+        frota[nome_navio] = [posicoes]
+    return frota
